0a1ae19a1f99424717fe6803c00ebfb9c62466d5
stassig <stassig164@gmail.com>
2023-04-16 18:58:18 +0200

    Dockerizing Next.js web app

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,13 @@
+const path = require("path");
+require("dotenv").config({ path: "../../.env.local" });
+
 module.exports = {
   reactStrictMode: true,
+  output: "standalone",
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
+  images: {
+    domains: ["fastly.picsum.photos"],
+  },
 };
