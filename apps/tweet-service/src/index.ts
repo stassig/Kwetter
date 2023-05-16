cef6e9ff04255bfef632146b1cbf45d577867f3c
stassig <78664231+stassig@users.noreply.github.com>
2023-05-16 13:32:12 +0200

    secrets added

--- apps/tweet-service/src/index.ts
+++ apps/tweet-service/src/index.ts
@@ -3,11 +3,12 @@
 import { createServer } from "./server";
 import { tweetRouter } from "./routes";
 
-const port = process.env.NEXT_PUBLIC_TWEET_SERVICE_URL || 3001;
+const port = 3001;
+const endPoint = process.env.NEXT_PUBLIC_TWEET_SERVICE_URL || "/tweet";
 const server = createServer();
 
 server.listen(port, () => {
   console.log(`api running on ${port}`);
 });
 
-server.use("/", tweetRouter());
+server.use(endPoint, tweetRouter());
