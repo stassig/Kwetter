d8ebaaca6cea3708d7193f3b072a08fed4e10acd
stassig <stassig164@gmail.com>
2023-06-08 23:54:08 +0200

    k8s local setup and k6 load testing & HPA added for tweet service

---
+++ k8s/local/loadtest.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '1m', target: 10 },    // ramp up to 10 users over 1 minute
+    { duration: '1m', target: 20 },    // ramp up to 20 users over 1 minute
+    { duration: '1m', target: 40 },    // ramp up to 40 users over 1 minute
+    { duration: '1m', target: 80 },    // ramp up to 80 users over 1 minute
+    { duration: '1m', target: 0 },     // ramp down to 0 users over 1 minute
+  ],
+};
+
+export default function () {
+  const res = http.get('http://localhost:8080/tweet');
+  sleep(1);
+}
