1c1af7411c9c2fbe8e9a97fe03cbd160ab91f02e
stassig <stassig164@gmail.com>
2023-06-14 15:17:17 +0200

    like & unfollow button added

--- apps/web/src/components/Timeline.tsx
+++ apps/web/src/components/Timeline.tsx
@@ -8,20 +8,42 @@ const Timeline = () => {
       datePosted: "June 14, 2023",
       message: "This is a tweet!",
       numLikes: 27,
+      username: "User1",
+      liked: false,
     },
     {
       userProfilePic: "/profile2.jpg",
       datePosted: "June 13, 2023",
       message: "Another tweet here.",
       numLikes: 14,
+      username: "User2",
+      liked: false,
     },
     // more tweets...
   ];
 
+  const handleUnfollow = (username: String) => {
+    console.log(`Unfollowed ${username}`);
+    // Handle the unfollow action here
+  };
+
+  const handleLike = async () => {
+    console.log("Liked!");
+    // Handle the like action here, e.g., make an API call to your backend
+    return Promise.resolve();
+  };
+
   return (
     <Container>
       {tweets.map((tweet, index) => (
-        <Tweet key={index} tweet={tweet} />
+        <Tweet
+          key={index}
+          tweet={{
+            ...tweet,
+            onUnfollow: () => handleUnfollow(tweet.username),
+            onLike: handleLike,
+          }}
+        />
       ))}
     </Container>
   );
--- apps/web/src/components/Tweet.tsx
+++ apps/web/src/components/Tweet.tsx
@@ -1,14 +1,32 @@
-import { Paper, Text, Avatar, Badge } from "@mantine/core";
-import { AiOutlineHeart } from "react-icons/ai";
+import { useState } from "react";
+import { Paper, Text, Avatar, Badge, Button } from "@mantine/core";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 
 interface TweetProps {
   userProfilePic: string;
   datePosted: string;
   message: string;
   numLikes: number;
+  username: string;
+  onUnfollow: () => void;
+  liked: boolean;
+  onLike: () => Promise<void>;
 }
 
 const Tweet = ({ tweet }: { tweet: TweetProps }) => {
+  const [likes, setLikes] = useState(tweet.numLikes);
+  const [liked, setLiked] = useState(tweet.liked);
+
+  const handleLike = async () => {
+    try {
+      await tweet.onLike();
+      setLikes(likes + 1);
+      setLiked(true);
+    } catch (error) {
+      console.error("Like operation failed:", error);
+    }
+  };
+
   return (
     <Paper style={{ marginBottom: "10px", padding: "10px" }} shadow="xs">
       <div style={{ display: "flex", marginBottom: "10px" }}>
@@ -18,16 +36,24 @@ const Tweet = ({ tweet }: { tweet: TweetProps }) => {
           radius="xl"
           style={{ marginRight: "10px" }}
         />
-        <div>
+        <div style={{ flex: 1 }}>
+          <Text weight={500} size="md">
+            {tweet.username}
+          </Text>
           <Text size="sm" color="dimmed">
             {tweet.datePosted}
           </Text>
           <Text>{tweet.message}</Text>
           <div style={{ display: "flex", alignItems: "center" }}>
-            <AiOutlineHeart style={{ marginRight: "5px" }} />
-            <Badge color="pink">{tweet.numLikes}</Badge>
+            <Button variant="link" onClick={handleLike}>
+              {liked ? <AiFillHeart color="red" /> : <AiOutlineHeart />}
+            </Button>
+            <Badge color="pink">{likes}</Badge>
           </div>
         </div>
+        <Button color="blue" onClick={tweet.onUnfollow}>
+          Unfollow
+        </Button>
       </div>
     </Paper>
   );
