2c7747401916d1abb0f21b5d222338ae3dc4835e
stassig <stassig164@gmail.com>
2023-06-15 16:00:16 +0200

    logo update

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -8,7 +8,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Page title</title>
+        <title>Kwetter</title>
+        <link rel="icon" href="/Original_Logo.png" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
