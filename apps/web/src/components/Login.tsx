ca7b10c42e210573c19fb1d703bb3d4217513a80
stassig <stassig164@gmail.com>
2023-06-18 12:06:29 +0200

    Landing page design fixed

--- apps/web/src/components/Login.tsx
+++ apps/web/src/components/Login.tsx
@@ -1,7 +1,7 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { Image, Paper } from "@mantine/core";
+import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@mantine/core";
 
@@ -19,57 +19,64 @@ const Login = () => {
   if (error) return <div>{error.message}</div>;
 
   return (
-    <div
-      style={{
-        backgroundImage: 'url("/Background.png")',
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        position: "relative",
-        height: "100vh",
-      }}
-    >
+    <div className="container">
+      <div style={{ position: "absolute", inset: 0, zIndex: -40 }}>
+        <Image
+          src="/Background.png"
+          alt="Description of the image"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        />
+      </div>
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
+          justifyContent: "center",
           alignItems: "center",
+          height: "100vh",
         }}
       >
         <div
           style={{
             display: "flex",
+            flexDirection: "column",
             alignItems: "center",
-            marginBottom: "20px",
           }}
         >
-          <Image
-            src="/Original_Logo.png"
-            alt="Description of the image"
-            width={100}
-            height={100}
-          />
           <div
             style={{
-              height: "70px", // adjust this to match text height
-              borderLeft: "2px solid #000",
-              marginLeft: "10px",
-              marginRight: "10px",
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "20px",
             }}
-          />
-          <div style={{ fontSize: "30px", fontWeight: "bold" }}>Kwetter</div>
+          >
+            <Image
+              src="/Original_Logo.png"
+              alt="Description of the logo image"
+              width={100}
+              height={100}
+            />
+            <div
+              style={{
+                height: "70px",
+                borderLeft: "2px solid #000",
+                marginLeft: "10px",
+                marginRight: "10px",
+              }}
+            />
+            <div style={{ fontSize: "30px", fontWeight: "bold" }}>Kwetter</div>
+          </div>
+          <Button
+            component={Link}
+            href="/api/auth/login"
+            size="xl"
+            radius="xl"
+            style={{ fontSize: "30px", padding: "15px 40px", width: "100%" }}
+          >
+            LOGIN
+          </Button>
         </div>
-        <Button
-          component={Link}
-          href="/api/auth/login"
-          size="xl"
-          radius="xl"
-          style={{ fontSize: "30px", padding: "15px 40px", width: "100%" }}
-        >
-          LOGIN
-        </Button>
       </div>
     </div>
   );
