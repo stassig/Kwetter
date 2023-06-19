392871dd97943d8d3be25edc9994b3445143d7cb
stassig <stassig164@gmail.com>
2023-06-19 13:04:25 +0200

    e2e testing

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,42 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.getByRole("link", { name: "LOGIN" }).click();
+  await page.getByLabel("Email address").click();
+  await page.getByLabel("Email address").fill("s.petkov@mail.bg");
+  await page.getByLabel("Password").click();
+  await page.getByLabel("Password").fill("ASdSDA12321!");
+  await page.getByRole("button", { name: "Continue", exact: true }).click();
+  await expect(page).toHaveURL(/.*home/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("post", async ({ page }) => {
+  await page.getByPlaceholder("What's on your mind, s.petkov?").click();
+  await page
+    .getByPlaceholder("What's on your mind, s.petkov?")
+    .fill("Hey, this is a test!");
+  await page.getByRole("button", { name: "Post" }).click();
+});
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+test("profile", async ({ page }) => {
+  await page.getByRole("link", { name: "Profile" }).click();
+  await expect(page).toHaveURL(/.*profile/);
+});
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+test("like", async ({ page }) => {
+  await page
+    .locator("div:nth-child(4) > .mantine-UnstyledButton-root")
+    .first()
+    .click();
+  await page
+    .locator(
+      "div:nth-child(3) > div > div:nth-child(2) > div:nth-child(4) > .mantine-UnstyledButton-root"
+    )
+    .click();
+  await page
+    .locator(
+      "div:nth-child(4) > div > div:nth-child(2) > div:nth-child(4) > .mantine-UnstyledButton-root"
+    )
+    .click();
 });
